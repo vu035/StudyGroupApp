7d47836d19a7067f67b7e683502fb9818cab0cdc
Jason Meyer <Mars@mars.attlocal.net>
2015-10-21 22:31:16 -0700

    added local rails db for dev use

---
+++ studygroup_rails/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Studygroup.create(department: "CS", class_number: 111)
+  Studygroup.create(department: "ENG" ,class_number: 211)
+  Studygroup.create(department: "MATH" , class_number: 101)
