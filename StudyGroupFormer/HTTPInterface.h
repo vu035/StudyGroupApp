808110d9f18e5a51b98f57ad69e43ebda97f40bf
Jason Meyer <Mars@mars.attlocal.net>
2015-10-30 00:03:57 -0700

    updated functs for new attributes

--- StudyGroupFormer/HTTPInterface.h
+++ StudyGroupFormer/HTTPInterface.h
@@ -4,8 +4,9 @@
 #include <QJsonArray>
 
 
-QJsonArray getRequest();
-void postCreateGroup(QString department, QString class_num);
+QJsonArray getAllGroups();
+void postCreateGroup(QString department, QString class_num, QString date, QString time);
+void postCreateUser(QString email, QString password, QString firstname, QString lastname, QString username);
 void postCreateUser(QString email, QString password);
 bool postLogin(QString email, QString password);
 
